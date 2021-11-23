2841eed2099db46daa091481c82386bf81d87af6
Vladislav <vlad356102@yandex.ru>
2021-11-23 13:17:50 +0300

    lil fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="footer">
-            <p className="footer__sign">© ${new Date().getFullYear()} Mesto Russia</p>
+            <p className="footer__sign">© {new Date().getFullYear()} Mesto Russia</p>
         </footer>
     );
 }
